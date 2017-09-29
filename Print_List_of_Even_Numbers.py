e214b4c03b95dda3b06d7ec0a0ac071130744679
Shantam Vijayputra <vshantam@yahoo.com>
2017-09-30 03:16:47 +0530

    Update Print_List_of_Even_Numbers.py

--- Print_List_of_Even_Numbers.py
+++ Print_List_of_Even_Numbers.py
@@ -1,4 +1,4 @@
 #user can give in put now
 #pyhton3
 
-print ([x for x in range(int(input()),int(input())) if not x%2])
+print ([x for x in range(int(input().strip()),int(input().strip())) if not x%2])
