8a622e23244d6d637d8b0884b3711e4b3211abbe
Shantam Vijayputra <vshantam@yahoo.com>
2017-10-06 02:35:26 +0530

    added classmethod and display

--- 4 Digit Number Combinations.py
+++ 4 Digit Number Combinations.py
@@ -1,7 +1,19 @@
 # ALL the combinations of 4 digit combo
-def FourDigitCombinations():
-    numbers=[]
-    for code in range(10000):
-        code=str(code).zfill(4)
-        print code,
-        numbers.append(code)
+class combinations(object):
+    def __init__(self,numbers=[]):
+        self.numbers=numbers
+    @classmethod
+    def FourDigitCombinations(self):
+        numbers=[]
+        for code in range(10000):
+            code=str(code).zfill(4)
+            print code,
+            numbers.append(code)
+        return numbers
+if __name__=='__main__':
+    obj=combinations()
+    res=obj.FourDigitCombination()
+    #Displaying all the combinations
+    for i in numbers:
+        print i ,
+  
